87aa84ca3d98d38ede1034c4c0711a4298fef4e7
isahilsachdev <isahilsachdev00@gmail.com>
2021-06-03 18:20:44 +0530

    auth redirect changes

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { AppBar, Typography, Toolbar, Avatar, Button } from '@material-ui/core';
-import { Link, useHistory, useLocation } from 'react-router-dom';
+import { Link, Redirect, useHistory, useLocation } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import decode from 'jwt-decode';
 
@@ -37,20 +37,52 @@ const Navbar = () => {
   }, [location]);
 
   return (
-    <AppBar className={classes.appBar} position="static" color="inherit">
-      <Link to="/" className={classes.brandContainer}>
-        <img component={Link} to="/" src={memoriesText} alt="icon" height="45px" />
-        <img className={classes.image} src={memoriesLogo} alt="icon" height="40px" />
+    <AppBar className={classes.appBar} position='static' color='inherit'>
+      <Link to='/' className={classes.brandContainer}>
+        <img
+          component={Link}
+          to='/'
+          src={memoriesText}
+          alt='icon'
+          height='45px'
+        />
+        <img
+          className={classes.image}
+          src={memoriesLogo}
+          alt='icon'
+          height='40px'
+        />
       </Link>
       <Toolbar className={classes.toolbar}>
         {user?.result ? (
           <div className={classes.profile}>
-            <Avatar className={classes.purple} alt={user?.result.name} src={user?.result.imageUrl}>{user?.result.name.charAt(0)}</Avatar>
-            <Typography className={classes.userName} variant="h6">{user?.result.name}</Typography>
-            <Button variant="contained" className={classes.logout} color="secondary" onClick={logout}>Logout</Button>
+            <Avatar
+              className={classes.purple}
+              alt={user?.result.name}
+              src={user?.result.imageUrl}
+            >
+              {user?.result.name.charAt(0)}
+            </Avatar>
+            <Typography className={classes.userName} variant='h6'>
+              {user?.result.name}
+            </Typography>
+            <Button
+              variant='contained'
+              className={classes.logout}
+              color='secondary'
+              onClick={logout}
+            >
+              Logout
+            </Button>
           </div>
         ) : (
-          <Button component={Link} to="/auth" variant="contained" color="primary">Sign In</Button>
+          <Button
+            onClick={() => history.push('/auth')}
+            variant='contained'
+            color='primary'
+          >
+            Sign In
+          </Button>
         )}
       </Toolbar>
     </AppBar>
